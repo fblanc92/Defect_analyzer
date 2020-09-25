10956428ab5d0341eb1dc807e62001e3e1a2056f
FedericoBlanc <flanc92@gmail.com>
2020-09-25 18:02:01 -0300

    Init function renamed

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 from resources.project_utils import get_coils_in_folder
 import os
-def init_analyzer():
+def init_app():
     coils_dict = get_coils_in_folder(r'c:/test')
     print(os.getcwd())
 
 
 if __name__ == '__main__':
-    init_analyzer()
+    init_app()
