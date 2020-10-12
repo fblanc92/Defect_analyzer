99985bd6a6338cbdf4e519e560ceee62de1ef0f8
FedericoBlanc <flanc92@gmail.com>
2020-10-11 23:28:13 -0300

    Added: areas detected by defect to the coil class

--- resources/classes/coil.py
+++ resources/classes/coil.py
@@ -1,4 +1,6 @@
 class Coil:
+    areas = {}
+
     def __init__(self, id, date, time, path, image_list):
         self.id = id
         self.date = date
@@ -6,3 +8,8 @@ class Coil:
         self.path = path
         self.image_list = image_list
 
+    def set_areas_from_dict(self, areas_dict):
+        self.areas = areas_dict
+
+    def get_areas(self):
+        return self.areas
