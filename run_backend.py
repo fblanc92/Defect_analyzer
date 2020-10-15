159619589a425ea9239c27ab1d962c544d5ee22a
FedericoBlanc <flanc92@gmail.com>
2020-10-15 15:56:15 -0300

    Added: Load of default thresholds Added: creation of path_coils_folder if not existing

--- run_backend.py
+++ run_backend.py
@@ -1,6 +1,8 @@
 import threading
+import json
+from collections import defaultdict
 from Defect_analyzer_back.resources.config.config_initializer import load_initial_config
-
+from Defect_analyzer_back.resources.config.configs_utils import create_folder_if_missing
 
 load_initial_config()
 
@@ -10,9 +12,22 @@ from Defect_analyzer_back.resources.coil_utils import get_coils_in_folder, updat
 from Defect_analyzer_back.resources.config.configs_utils import get_current_config_json
 
 
+def load_default_thresholds():
+    path_to_labels_json = get_current_config_json()['config']['path_to_labels_json']
+    default_thresholds_dict = defaultdict(lambda: defaultdict())
+    with open(path_to_labels_json) as f:
+        labels_json = json.load(f)
+    for item in labels_json['items']:
+        default_thresholds_dict[item['name']] = 1 * 10 ** 9
+    with open(get_current_config_json()['config']['path_to_current_config_json']) as f:
+        current_config_json = json.load(f)
+        current_config_json['thresholds'] = default_thresholds_dict
+    with open(get_current_config_json()['config']['path_to_current_config_json'],'w') as g:
+        json.dump(current_config_json, g, indent=2)
+
 def init_register():
-    current_config_json = get_current_config_json()
-    coils_in_folder = get_coils_in_folder(current_config_json['config']['path_coils_folder'])
+    create_folder_if_missing(get_current_config_json()['config']['path_coils_folder'])
+    coils_in_folder = get_coils_in_folder(get_current_config_json()['config']['path_coils_folder'])
     update_coil_register(coils_in_folder)
 
 
@@ -30,9 +45,8 @@ def start_app():
     app_timer = threading.Timer(current_config_json['config']['scan_timer_delay'], start_app).start()
 
 
-
 def start_backend():
     # if __name__ == '__main__':
-
     init_register()
+    load_default_thresholds()
     start_app()
